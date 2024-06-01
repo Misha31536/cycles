38f56630017dbb892a41aa631005b25bdb9ef174
Misha31536 <e4ik@ya.ru>
2024-06-01 23:19:01 +0700

    Add files via upload
    убрал ненужную строчку с прошлой версии

--- cycles.py
+++ cycles.py
@@ -1,7 +1,6 @@
 first = int(input("Ввведите первое число "))
 second = int(input("Введите второе число "))
 third = int(input("Введите третье число "))
-quantity =int(1)
 if first == second and first == third:
     print("одинаковых чисел 3")
 elif second == third or first == third or first == second:
