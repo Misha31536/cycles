4d1d0e66cb8d43b275faae9a401421c813891ebb
Misha31536 <e4ik@ya.ru>
2024-06-01 23:16:04 +0700

    Add files via upload
    Выполнил с помощью операторов and  и or

--- cycles.py
+++ cycles.py
@@ -2,13 +2,10 @@ first = int(input("Ввведите первое число "))
 second = int(input("Введите второе число "))
 third = int(input("Введите третье число "))
 quantity =int(1)
-if first == second:
-    quantity = quantity + 1
-    if first == third:
-        quantity = quantity +1
-    print("одинаковых чисел ", quantity)
-elif second == third:
-    print("одинаковых чисел ", quantity + 1)
+if first == second and first == third:
+    print("одинаковых чисел 3")
+elif second == third or first == third or first == second:
+    print("одинаковых чисел 2")
 else:
-    print("одинаковых чисел ", quantity -1)
+    print("одинаковых чисел 0")
 
